f14948d173e89f83d213b33ac5f2a5c3758f3750
michael.luethi <michael.luethi@swissq.it>
2022-08-08 14:06:31 +0200

    Backup

--- ISK/src/app/lib/infringement-without-obv/infringement-without-obv.component.ts
+++ ISK/src/app/lib/infringement-without-obv/infringement-without-obv.component.ts
@@ -1,72 +1,65 @@
-import { InspectionPoint } from '../../../../../ISK/src/app/lib/inspection-point';
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import {
-  FormArray,
-  FormControl,
-  FormGroup,
-  FormGroupDirective,
-} from '@angular/forms';
-import { Subject, takeUntil } from 'rxjs';
-import { Infringement } from '../../../../../ISK/src/app/lib/infringement';
+import {InspectionPoint} from '../../../../../ISK/src/app/lib/inspection-point';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {FormArray, FormControl, FormGroup, FormGroupDirective} from '@angular/forms';
+import {Subject, takeUntil} from 'rxjs';
+import {Infringement} from '../../../../../ISK/src/app/lib/infringement';
 
 @Component({
-  selector: 'app-infringement-without-obv',
-  templateUrl: './infringement-without-obv.component.html',
-  styleUrls: ['./infringement-without-obv.component.scss'],
+	selector: 'app-infringement-without-obv',
+	templateUrl: './infringement-without-obv.component.html',
+	styleUrls: ['./infringement-without-obv.component.scss']
 })
 export class InfringementWithoutObvComponent implements OnInit, OnDestroy {
-  form!: FormGroup;
-  @Input() inspectionPoint!: InspectionPoint;
-  destroy$: Subject<boolean> = new Subject<boolean>();
-  constructor(private readonly rootFormGroup: FormGroupDirective) {}
+	form!: FormGroup;
+	@Input() inspectionPoint!: InspectionPoint;
+	destroy$: Subject<boolean> = new Subject<boolean>();
+	constructor(private readonly rootFormGroup: FormGroupDirective) {}
 
-  ngOnInit(): void {
-    this.form = this.rootFormGroup.control.get(
-      `${this.inspectionPoint.name}Group`
-    ) as FormGroup;
+	ngOnInit(): void {
+		this.form = this.rootFormGroup.control.get(`${this.inspectionPoint.name}Group`) as FormGroup;
 
-    if (this.infringements !== null) {
-      this.form
-        .get(this.inspectionPoint.name)
-        ?.valueChanges.pipe(takeUntil(this.destroy$))
-        .subscribe((value) => {
-          if (value === '1' && this.infringements.length === 0) {
-            this.addInfringement();
-          } else {
-            this.clearInfringements();
-          }
-        });
-    }
-  }
+		if (this.infringements !== null) {
+			this.form
+				.get(this.inspectionPoint.name)
+				?.valueChanges.pipe(takeUntil(this.destroy$))
+				.subscribe(value => {
+					if (value === '1' && this.infringements.length === 0) {
+						this.addInfringement();
+					} else {
+						this.clearInfringements();
+					}
+				});
+		}
+	}
 
-  ngOnDestroy(): void {
-    this.destroy$.next(true);
-    this.destroy$.unsubscribe();
-  }
+	ngOnDestroy(): void {
+		this.destroy$.next(true);
+		this.destroy$.unsubscribe();
+	}
 
-  get infringements() {
-    return this.form.get(
-      `${this.inspectionPoint.name}Infringements`
-    ) as FormArray;
-  }
+	get infringements() {
+		return this.form.get(`${this.inspectionPoint.name}Infringements`) as FormArray;
+	}
 
-  get canAddSecondInfringementClassification() {
-    const infringementsClasses = this.inspectionPoint.infringements;
-    const group = infringementsClasses.reduce((group, infringement) => {
-      if (!group.some(infringement.exclusionGroup ?? number.))
+	get canAddSecondInfringementClassification() {
+		const infringementsClasses = this.inspectionPoint.infringements;
+		const groupCodes = infringementsClasses.map(i => i.exclusionGroup ?? NaN);
+		const group = [] as number[];
 
-      return group;
-    }, [] as number[]);
+		groupCodes.forEach(gc => {
+			if (!group.includes(gc)) {
+				group.push(gc);
+			}
+		});
 
-    console.log(JSON.stringify(group));
-    return Object.keys(group).length > 1;
-  }
+		return group.length > 1;
+	}
 
-  addInfringement() {
-    this.infringements.push(new FormControl('0'));
-  }
+	addInfringement() {
+		this.infringements.push(new FormControl('0'));
+	}
 
-  clearInfringements() {
-    this.infringements.clear();
-  }
+	clearInfringements() {
+		this.infringements.clear();
+	}
 }
