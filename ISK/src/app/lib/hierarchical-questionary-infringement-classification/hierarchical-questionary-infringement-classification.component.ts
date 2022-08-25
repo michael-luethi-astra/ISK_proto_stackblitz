5fbe1239502d7e1ebfdd366b1030515751009fd4
michael.luethi <michael.luethi@swissq.it>
2022-08-25 10:25:35 +0200

    breaktrough

--- ISK/src/app/lib/hierarchical-questionary-infringement-classification/hierarchical-questionary-infringement-classification.component.ts
+++ ISK/src/app/lib/hierarchical-questionary-infringement-classification/hierarchical-questionary-infringement-classification.component.ts
@@ -1,6 +1,5 @@
-import {KeyValue} from '@angular/common';
 import {Component, Input, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {ControlValueAccessor, FormBuilder, FormControl, FormGroup, NG_VALUE_ACCESSOR} from '@angular/forms';
 
 export interface HierarchyElement {
 	header?: string;
@@ -17,19 +16,106 @@ export interface RadioItem {
 @Component({
 	selector: 'app-hierarchical-questionary-infringement-classification',
 	templateUrl: './hierarchical-questionary-infringement-classification.component.html',
-	styleUrls: ['./hierarchical-questionary-infringement-classification.component.scss']
+	styleUrls: ['./hierarchical-questionary-infringement-classification.component.scss'],
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			multi: true,
+			useExisting: HierarchicalQuestionaryInfringementClassificationComponent
+		}
+	]
 })
-export class HierarchicalQuestionaryInfringementClassificationComponent implements OnInit {
+export class HierarchicalQuestionaryInfringementClassificationComponent implements OnInit, ControlValueAccessor {
 	@Input() root!: HierarchyElement;
-	rootSelection?: number = undefined;
+	formGroupElements = [] as string[];
+	selectionPath?: number[] = undefined;
 	formGroup!: FormGroup;
+
+	touched = false;
+	disabled = false;
+	selection?: number;
 
 	constructor(readonly fb: FormBuilder) {}
 
+	onChange = (quantity: number) => {};
+
+	onTouched = () => {};
+
+	writeValue(obj: string): void {
+		throw new Error('Method not implemented.');
+	}
+
+	registerOnChange(onChange: any) {
+		this.onChange = onChange;
+	}
+
+	registerOnTouched(onTouched: any) {
+		this.onTouched = onTouched;
+	}
+
 	ngOnInit(): void {
-		console.log(this.rootChilds);
+		this.setupFormGroup();
+
+		this.formGroup.valueChanges.subscribe(o => {
+			console.log(JSON.stringify(o));
+
+			this.selectionPath = [];
+			let childArray = this.root.childs!;
+
+			for (let index = 0; index < this.formGroupElements.length; index++) {
+				const elementName = this.formGroupElements[index];
+				const selectionValue = o[elementName];
+
+				if (selectionValue !== '') {
+					const selectionValueNumber = (selectionValue as number) - 1;
+					const element = childArray[selectionValueNumber];
+
+					if (element.childs !== undefined) {
+						this.selectionPath.push(selectionValueNumber);
+						console.log(this.selectionPath);
+						childArray = element.childs;
+					} else {
+						console.log(selectionValueNumber);
+						this.setValue(selectionValueNumber);
+						break;
+					}
+				}
+			}
+		});
+	}
+
+	setValue(value: number) {
+		this.markAsTouched();
+		if (!this.disabled) {
+			this.selection = value;
+			this.onChange(this.selection);
+		}
+	}
+
+	getDepth(element: HierarchyElement, count?: number): number {
+		const depths = element.childs?.map(e => this.getDepth(e, count !== undefined ? count + 1 : 0));
+		return depths ? Math.max(...depths) : count ?? 0;
+	}
+
+	private setupFormGroup() {
 		this.formGroup = this.fb.group({root: ['']});
-		this.formGroup.valueChanges.subscribe(o => console.log(o));
+		this.formGroupElements.push('root');
+
+		for (let index = 0; index < this.getDepth(this.root); index++) {
+			const elementName = `child${index}`;
+			this.formGroup.addControl(elementName, new FormControl(''));
+			this.formGroupElements.push(elementName);
+		}
+	}
+
+	get activePath() {
+		let path = [] as HierarchyElement[];
+
+		if (this.selectionPath) {
+			path = this.getActiveElement(this.selectionPath);
+		}
+
+		return path;
 	}
 
 	get rootHeader() {
@@ -42,5 +128,27 @@ export class HierarchicalQuestionaryInfringementClassificationComponent implemen
 				return child.shortName ?? '';
 			}) ?? ([] as string[])
 		);
+	}
+
+	private markAsTouched() {
+		if (!this.touched) {
+			this.onTouched();
+			this.touched = true;
+		}
+	}
+
+	private getActiveElement(pathIndexes: number[]): HierarchyElement[] {
+		const path = [] as HierarchyElement[];
+
+		let childArray = this.root.childs!;
+
+		for (let index = 0; index < pathIndexes.length; index++) {
+			const element = childArray[pathIndexes[index]];
+			path.push(element);
+
+			childArray = element.childs!;
+		}
+
+		return path;
 	}
 }
